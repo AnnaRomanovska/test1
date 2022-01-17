1b575ceb9f264f02f528462b0510274d7f85f598
Anna Romanovska <anna.romanovska1@resmed.com>
2022-01-17 16:26:42 +0200

    new brabch

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,14 +13,10 @@ public class main {
 
         ChromeDriver driver = new ChromeDriver();
                 driver.get ("https://allo.ua");
-        WebElement test1 = driver.findElement(By.xpath("//div[@class='ct-button']"));
-                test1.click();
-                WebElement test2 = driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/header/div[1]/div[2]/div[2]/div[2]/div/ul/li[1]/a"));
-                test2.click();
-                WebElement test3 = driver.findElement(By.xpath("//li[@class='portal-card__link-container'][1]"));
-                test3.click();
-                WebElement test4 = driver.findElement(By.xpath("//button[text()='Купити'][1]"));
-                test4.click();
+        WebElement test1 = driver.findElement(By.cssSelector("#search-form__input"));
+             test1.sendKeys("Xiaomi", Keys.ENTER);
+        WebElement test4 = driver.findElement(By.xpath("//button[text()='Купити'][1]"));
+             test4.click();
 
 
 
